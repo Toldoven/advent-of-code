634b32ac4eff70df6a19d4cbe688eeacd4c88ce5
Toldoven <toldoven@proton.me>
2024-09-04 19:00:10 +0300

    2016: Day 3

---
+++ src/year2016/day03/Y2016D03.kt
@@ -0,0 +1,33 @@
+package year2016.day03
+
+import framework.InputProvider
+import framework.solution
+import utils.convertRowToColumn
+
+
+typealias Triangle = List<Int>
+
+
+fun main() = solution(2016, 3, "Squares With Three Sides") {
+
+    val inputRegex = Regex("\\S+")
+
+    fun InputProvider.parseInput() = lines.map { line ->
+        inputRegex.findAll(line).map { it.value.toInt() }.toList()
+    }
+
+    fun List<Triangle>.countValidTriangles() = count {
+        val max = it.withIndex().maxBy { (_, value) -> value }
+        it.filterIndexed { index, _ -> index != max.index }.sum() > max.value
+    }
+
+    partOne {
+        parseInput().countValidTriangles()
+    } 
+    
+    partTwo {
+        parseInput().chunked(3)
+            .flatMap { chunk -> chunk.convertRowToColumn() }
+            .countValidTriangles()
+    }
+}
