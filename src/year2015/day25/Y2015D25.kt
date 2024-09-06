f03e8943d2e7d715d0679d702dbe9dcff5f96279
Toldoven <toldoven@proton.me>
2024-09-06 06:59:09 +0300

    2015: Day 25

---
+++ src/year2015/day25/Y2015D25.kt
@@ -0,0 +1,24 @@
+package year2015.day25
+
+import framework.InputProvider
+import framework.solution
+import utils.IntVec2
+import utils.dbg
+
+fun main() = solution(2015, 25, "Let It Snow") {
+
+    val regex =
+        Regex("""To continue, please consult the code grid in the manual.\s*Enter the code at row (\d+), column (\d+).""")
+
+    fun InputProvider.parseInput() = regex.matchEntire(input.dbg())?.let {
+        IntVec2(it.groupValues[1].toInt(), it.groupValues[2].toInt())
+    } ?: throw IllegalArgumentException("Invalid input")
+
+
+    partOne {
+        val (x, y) = parseInput()
+        val flatIndex = (x + y - 2) * (x + y - 1) / 2 + y - 1
+        generateSequence(20151125L) { it * 252533L % 33554393L }.elementAt(flatIndex)
+    } 
+
+}
