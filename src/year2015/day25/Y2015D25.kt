f82dab942057a9c525deeb8cbfa102bf2e882a63
Toldoven <toldoven@proton.me>
2024-09-08 06:38:49 +0300

    2016: Day 13

--- src/year2015/day25/Y2015D25.kt
+++ src/year2015/day25/Y2015D25.kt
@@ -3,14 +3,15 @@ package year2015.day25
 import framework.InputProvider
 import framework.solution
 import utils.IntVec2
-import utils.dbg
+
+
 
 fun main() = solution(2015, 25, "Let It Snow") {
 
     val regex =
         Regex("""To continue, please consult the code grid in the manual.\s*Enter the code at row (\d+), column (\d+).""")
 
-    fun InputProvider.parseInput() = regex.matchEntire(input.dbg())?.let {
+    fun InputProvider.parseInput() = regex.matchEntire(input)?.let {
         IntVec2(it.groupValues[1].toInt(), it.groupValues[2].toInt())
     } ?: throw IllegalArgumentException("Invalid input")
 
---
+++ src/year2016/day13/Y2016D13.kt
@@ -0,0 +1,90 @@
+package year2016.day13
+
+import framework.InputProvider
+import framework.solution
+import utils.IntVec2
+
+data class Path(val currentPosition: IntVec2, val visited: Set<IntVec2> = setOf(currentPosition))
+
+class Maze(private val seed: Int) {
+    private fun isWall(index: IntVec2): Boolean {
+        val (x, y) = index
+
+        if (x < 0 || y < 0) {
+            return true
+        }
+
+        val value = (x * x + 3 * x + 2 * x * y + y + y * y) + seed
+        return value.countOneBits() % 2 == 1
+    }
+
+    private fun Path.visitAll(): Sequence<Path> = currentPosition
+        .adjacent4Way
+        .asSequence()
+        .filterNot { isWall(it) || visited.contains(it) }
+        .map { Path(it, visited + it) }
+
+
+    private fun bfsSequence() = generateSequence(listOf(Path(IntVec2(1, 1)))) { pathList ->
+        pathList.flatMap { it.visitAll() }
+    }
+
+    fun findShortestPath(
+        targetPosition: IntVec2,
+    ) = bfsSequence().indexOfFirst { pathList ->
+        pathList.any { it.currentPosition == targetPosition }
+    }
+
+    fun distinctCoordinates(steps: Int) = bfsSequence()
+        .drop(1)
+        .take(steps)
+        .flatten()
+        .flatMap { it.visited }
+        .distinct()
+        .count()
+
+    fun asString() = (0..6).joinToString("\n") { y ->
+        (0..9).joinToString("") { x ->
+            val index = IntVec2(x, y)
+            if (isWall(index)) "#" else "."
+        }
+    }
+}
+
+
+
+fun main() = solution(2016, 13, "A Maze of Twisty Little Cubicles") {
+
+    fun InputProvider.parseSeed() = input.toInt()
+
+    partOne {
+        Maze(parseSeed()).findShortestPath(
+            IntVec2(31, 39),
+        )
+    }
+
+    partOneTest {
+        "10" with {
+            val maze = Maze(parseSeed())
+            maze.asString()
+        } shouldOutput """
+            .#.####.##
+            ..#..#...#
+            #....##...
+            ###.#.###.
+            .##..#..#.
+            ..##....#.
+            #...##.###
+        """.trimIndent()
+
+        "10" with {
+            Maze(parseSeed()).findShortestPath(
+                IntVec2(7, 4),
+            )
+        } shouldOutput 11
+    }
+    
+    partTwo {
+        Maze(parseSeed()).distinctCoordinates(50)
+    }
+}
